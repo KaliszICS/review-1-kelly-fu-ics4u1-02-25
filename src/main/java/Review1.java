b42182740152b8819d2c3d941b4176dd845e0b1e
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-05 05:23:56 +0000

    Update program

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,23 +2,31 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
-		
+		System.out.println(“There once was a man from St. Ives.”);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		System.out.println(9/5);
+		System.out.println(9%5);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello \n Hello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean trueFalse = true;
+		System.out.println(trueFalse);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String word1 = "Hello";
+		String word2 = "World";
+		System.out.println(word1+" "+word2);
 	}
 
 }
